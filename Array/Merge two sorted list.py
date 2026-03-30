6ccf5493b64f80a285f566ca60db48ffbf72b9d9
Supriyo <supriyopal554@gmail.com>
2026-03-30 22:49:04 +0530

    fixed Merge two sorted array.py

--- Array/Merge two sorted list.py
+++ Array/Merge two sorted list.py
@@ -8,3 +8,41 @@ for num in l:
 
 key_value=frequncy_map.keys()
 print(key_value)
+
+#Another Method (optimal)
+    
+n = [1,1,1,2,3,4,5]
+m = [2,3,6,7,8,8,9]
+
+i = 0
+j = 0
+result = []
+l1 = len(n)
+l2 = len(m)
+
+while i < l1 and j < l2:
+    if n[i] < m[j]:
+        val = n[i]
+        i += 1
+    elif m[j] < n[i]:
+        val = m[j]
+        j += 1
+    else:
+        val = n[i]
+        i += 1
+        j += 1
+
+    if len(result) == 0 or result[-1] != val:
+        result.append(val)
+
+while i < l1:
+    if len(result) == 0 or result[-1] != n[i]:
+        result.append(n[i])
+    i += 1
+
+while j < l2:
+    if len(result) == 0 or result[-1] != m[j]:
+        result.append(m[j])
+    j += 1
+
+print(result)
