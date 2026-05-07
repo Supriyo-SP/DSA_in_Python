477582cf99760e5bf33bddf17e8f94aaa1d24678
Supriyo <supriyopal554@gmail.com>
2026-05-07 17:34:15 +0530

    fix the code remove linked list.py

--- Linked List/Remove Linked List Elements.py
+++ Linked List/Remove Linked List Elements.py
@@ -1,24 +1,43 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def build_list(values):
+    dummy = ListNode(0)
+    current = dummy
+    for value in values:
+        current.next = ListNode(value)
+        current = current.next
+    return dummy.next
+
+
+def list_to_pylist(head):
+    values = []
+    current = head
+    while current is not None:
+        values.append(current.val)
+        current = current.next
+    return values
 class Solution(object):
     def removeElements(self, head, val):
         if head is None:
             return head
-        dummy=ListNode(0)
-        dummy.next=head
-        temp=dummy
+        dummy = ListNode(0)
+        dummy.next = head
+        temp = dummy
         while temp.next is not None:
-            if temp.next.val==val:
-                temp.next=temp.next.next 
-            else:    
-                temp=temp.next    
-        return dummy.next             
-        """
-        :type head: Optional[ListNode]
-        :type val: int
-        :rtype: Optional[ListNode]
-        """
+            if temp.next.val == val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        return dummy.next
+
+
+if __name__ == "__main__":
+    head = build_list([1, 2, 6, 3, 4, 5, 6])
+    val = 6
+    result = Solution().removeElements(head, val)
+    print(list_to_pylist(result))
         
