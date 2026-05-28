94cbe42c44d87c120f259a1e8631e909cbadf516
Supriyo <supriyopal554@gmail.com>
2026-05-28 10:24:32 +0530

    leetcode : 14 (easy)

---
+++ strings/14. Longest Common Prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs)==0:
+            return ""
+        base=strs[0]
+        result=""
+        for i in range(0,len(base)):
+            for word in strs[1:]:
+                if i==len(word) or word[i]!=base[i]:
+                    return result
+            result+=base[i]
+        return result                
+        
