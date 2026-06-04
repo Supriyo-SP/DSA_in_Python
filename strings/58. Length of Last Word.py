8fcca95b31ece3589c8595fe64d3ff963b71fa8c
Supriyo <supriyopal554@gmail.com>
2026-06-04 22:06:06 +0530

    leetcode: 58 (easy)

---
+++ strings/58. Length of Last Word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = s.split()
+        return len(result[len(result)-1])
+        
