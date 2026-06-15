3df0f9f949b9d8ed891edfd43bd5f6d7d08e80d4
Supriyo <supriyopal554@gmail.com>
2026-06-15 22:56:53 +0530

    linked-list: add basic list demo

--- Linked List/1.0 linkedList.py
+++ Linked List/1.0 linkedList.py
@@ -1,10 +1,36 @@
-class Node:
-    def __init__(self,val):
-        self.val=val
-        self.next=None
+"""Basic linked list node demo."""
 
-node1=Node(2)
-node2=Node(4)
-node1.next=node2
-print(node1.next)
-print(node2)
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def build_list(values):
+    dummy = ListNode(0)
+    current = dummy
+    for value in values:
+        current.next = ListNode(value)
+        current = current.next
+    return dummy.next
+
+
+def list_to_pylist(head):
+    values = []
+    current = head
+    while current is not None:
+        values.append(current.val)
+        current = current.next
+    return values
+
+
+if __name__ == "__main__":
+    node1 = ListNode(2)
+    node2 = ListNode(4)
+    node1.next = node2
+    print(node1.next.val)
+    print(node2.val)
+
+    head = build_list([1, 2, 3])
+    print(list_to_pylist(head))
