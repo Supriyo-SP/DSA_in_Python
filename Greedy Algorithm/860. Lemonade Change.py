ae7d0768e31579e54146d849d0e9dd3ab9a33cc1
Supriyo <supriyopal554@gmail.com>
2026-08-17 20:35:47 +0530

    leetcode : 860 (easy)

---
+++ Greedy Algorithm/860. Lemonade Change.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five,ten = 0,0        
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                five+=1
+            elif bills[i] == 10:
+                if five >= 1:
+                    ten+=1
+                    five-=1
+                else:
+                    return False
+            else:
+                if ten >= 1 and five >=1:
+                    five -=1
+                    ten -=1
+                elif five >=3:
+                    five -=3
+                else:
+                    return False
+        return True
+
+
+        
